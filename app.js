46e0d121ff1e3f5a15a3456da96406dbd487786f
Cornelius Egberaneiye <corneliusajayi123@gmail.com>
2023-02-23 10:09:00 +0100

    made the syllabus part responsive as well

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ let exitMenuBtn = document.querySelector(".exitMenuBtn")
 
 function openMenu(){
     navBar.classList.add("blockMenu");
+    navBar.classList.remove("hidden")
     MenuBtn.style.display = "none";
     // console.log(menuBtn.style.display)
 }
 function closeMenu(){
     navBar.classList.remove("blockMenu");
+    navBar.classList.add("hidden")
     MenuBtn.style.display = "flex";
 }
