b38a176a2c6fbc42ec959026330d3e163095db6c
Cornelius Egberaneiye <corneliusajayi123@gmail.com>
2023-02-25 14:33:56 +0100

    added bg image to homepage and added a dropdown menu to studyAbroad

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let navBar = document.querySelector(".navBar")
 let MenuBtn = document.querySelector(".MenuBtn")
 let exitMenuBtn = document.querySelector(".exitMenuBtn")
+let StudyAbroadNav = document.querySelector(".StudyAbroadNav")
+let studyAbroadDropDown = document.querySelector(".studyAbroadDropDown")
 
 function openMenu(){
     navBar.classList.add("blockMenu");
@@ -12,4 +14,14 @@ function closeMenu(){
     navBar.classList.remove("blockMenu");
     navBar.classList.add("hidden")
     MenuBtn.style.display = "flex";
+}
+
+function showSAdropDown(){
+    studyAbroadDropDown.classList.remove("hidden")
+    studyAbroadDropDown.classList.add("flex")
+    // alert("i work")
+}
+function closeSAdropDown(){
+    studyAbroadDropDown.classList.add("hidden")
+    studyAbroadDropDown.classList.remove("flex")
 }
